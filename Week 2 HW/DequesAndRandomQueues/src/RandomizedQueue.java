45597d74605931ea43b4a05c0fae0406895dcbc4
Darah <darahfumadac@gmail.com>
2017-05-12 21:27:07 +0800

    Add RandomizedQueue class

---
+++ Week 2 HW/DequesAndRandomQueues/src/RandomizedQueue.java
@@ -0,0 +1,64 @@
+
+import java.util.Iterator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Darah
+ */
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    
+    private Item[] randomQueue;
+    
+   // construct an empty randomized queue
+    public RandomizedQueue()
+    {
+        randomQueue = (Item[])new Object[1];
+    }
+    
+    private class Node
+    
+    // is the queue empty?
+    public boolean isEmpty()
+    {
+    }
+    
+    // return the number of items on the queue
+    public int size()
+    {
+    }
+    
+    // add the item
+    public void enqueue(Item item)
+    {
+    
+    }
+    
+    // remove and return a random item
+    public Item dequeue()
+    {
+    
+    }
+    
+    // return (but do not remove) a random item
+    public Item sample()
+    {
+    
+    }
+    
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator()
+    {
+    
+    }
+    
+    
+    // unit testing (optional)
+    public static void main(String[] args)
+    {}
+}
