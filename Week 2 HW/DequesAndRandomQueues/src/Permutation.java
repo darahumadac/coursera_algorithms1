db6ee456bb512ea57d06369b1f2db22478feda54
Darah <darahfumadac@gmail.com>
2017-05-13 10:42:46 +0800

    Implementation for Permutation test client

---
+++ Week 2 HW/DequesAndRandomQueues/src/Permutation.java
@@ -0,0 +1,36 @@
+
+import edu.princeton.cs.algs4.StdIn;
+import java.util.Iterator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Darah
+ */
+public class Permutation {
+    public static void main(String[] args)
+    {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> randomQueue = new RandomizedQueue<>();
+        
+        String[] stringSequence = StdIn.readAllStrings();
+        for(String content : stringSequence)
+        {
+            randomQueue.enqueue(content);
+        }
+        
+        Iterator<String> iterator = randomQueue.iterator();
+        
+        int counter = 0;
+        while(counter != k && iterator.hasNext())
+        {
+            System.out.println(iterator.next());
+            counter++;
+        }
+    }
+}
