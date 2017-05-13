f6bad1fb40a48f9bb511f9b6781f95b7a021d4c1
Darah <darahfumadac@gmail.com>
2017-05-13 09:03:57 +0800

    - Added enqueue, dequeue, iterator implementations - Changed initialization of empty queue

--- Week 2 HW/DequesAndRandomQueues/src/RandomizedQueue.java
+++ Week 2 HW/DequesAndRandomQueues/src/RandomizedQueue.java
@@ -1,5 +1,7 @@
 
+import edu.princeton.cs.algs4.StdRandom;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,49 +16,120 @@ import java.util.Iterator;
 public class RandomizedQueue<Item> implements Iterable<Item> {
     
     private Item[] randomQueue;
+    private int size;
+    private int initialSize = 2;
     
    // construct an empty randomized queue
     public RandomizedQueue()
     {
-        randomQueue = (Item[])new Object[1];
+        size = 0;
+        randomQueue = (Item[])new Object[initialSize];
     }
-    
-    private class Node
     
     // is the queue empty?
     public boolean isEmpty()
     {
+        return size == 0;
     }
     
     // return the number of items on the queue
     public int size()
     {
+        return size;
     }
     
     // add the item
     public void enqueue(Item item)
     {
+        if(item == null)
+        {
+            throw new NullPointerException();
+        }
+        
+        randomQueue[size] = item;
+        size++;
+        
+        if(size == randomQueue.length)
+        {
+            resize(2 * size);
+        }
+    }
     
+    private void resize(int newSize)
+    {
+        Item[] tempQueue = (Item[])new Object[newSize];
+        
+        for(int i = 0; i < size; i++)
+        {
+            tempQueue[i] = randomQueue[i];
+        }
+        
+        randomQueue = tempQueue;
     }
     
     // remove and return a random item
     public Item dequeue()
     {
+        checkIfEmpty();
+        int index = StdRandom.uniform(size);
+        Item randomItem = randomQueue[index];
+        int lastIndex = size-1;
+        
+        if(index < lastIndex)
+        {
+            randomQueue[index] = randomQueue[lastIndex];
+        }
+        
+        randomQueue[lastIndex] = null;
+        size--;
+        
+        if(size < randomQueue.length/4)
+        {
+            resize(randomQueue.length/2);
+        }
+        
+        return randomItem;
+    }
     
+    private void checkIfEmpty()
+    {
+        if(size == 0)
+        {
+            throw new NoSuchElementException();
+        }
     }
     
     // return (but do not remove) a random item
     public Item sample()
     {
-    
+        checkIfEmpty();
+        int index = StdRandom.uniform(size);
+        return randomQueue[index];
     }
     
     // return an independent iterator over items in random order
     public Iterator<Item> iterator()
     {
-    
+        return new RandomQueueIterator();
     }
     
+    private class RandomQueueIterator implements Iterator<Item>
+    {
+        @Override
+        public boolean hasNext() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Item next() {
+            if(!hasNext())
+            {
+                throw new NoSuchElementException();
+            }
+            
+        }
+        
+    }
     
     // unit testing (optional)
     public static void main(String[] args)
