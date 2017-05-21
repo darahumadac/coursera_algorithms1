8665bd5329c0f0aa869a0a54387c9936444de9d5
Darah <darahfumadac@gmail.com>
2017-05-21 22:24:19 +0800

    Initial checkin

---
+++ Week 4 HW/8Puzzle/src/Solver.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Darah
+ */
+public class Solver {
+    
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial)
+    {
+    
+    }
+    
+    // is the initial board solvable?
+    public boolean isSolvable()
+    {
+    
+    }
+    
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves()
+    {
+    
+    }
+    
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution()
+    {
+    
+    }
+    
+    // solve a slider puzzle (given below)
+    public static void main(String[] args)
+    {
+    
+    }
+}
