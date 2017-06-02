9d2aacba5c344aa9696ae725a1ada808734086cf
Darah <darahfumadac@gmail.com>
2017-06-02 22:16:04 +0800

    Added PointSET methods implementation

--- Week 5 HW/KDTrees/src/PointSET.java
+++ Week 5 HW/KDTrees/src/PointSET.java
@@ -1,6 +1,8 @@
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+import java.util.ArrayList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,52 +16,76 @@ import edu.princeton.cs.algs4.RectHV;
  */
 public class PointSET {
     
+    private SET<Point2D> points;
+    
     // construct an empty set of points 
     public PointSET()
     {
-    
+        points = new SET<>();
     }
     
     // is the set empty? 
     public boolean isEmpty()
     {
-        return false;
+        return points.isEmpty();
     }
     
     // number of points in the set 
     public int size()
     {
-        return 0;
+        return points.size();
     }
     
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p)
-    {
-        
+    {   
+        points.add(p);
     }
     
     // does the set contain point p? 
     public boolean contains(Point2D p)
     {
-        return false;
+        return points.contains(p);
     }
     
     // draw all points to standard draw 
     public void draw()
     {
-    
+        for (Point2D point : points)
+        {
+            point.draw();
+        }
     }
     
     // all points that are inside the rectangle 
     public Iterable<Point2D> range(RectHV rect)
     {
-        return null;
+        ArrayList<Point2D> pointsInRect = new ArrayList<>();
+        for (Point2D point : points)
+        {
+            if (rect.contains(point))
+            {
+                pointsInRect.add(point);
+            }
+        }
+        
+        return pointsInRect;
     }
     
     // a nearest neighbor in the set to point p; null if the set is empty 
     public Point2D nearest(Point2D p)
     {
-        return null;
+        Point2D nearestNeighbor = null;
+        for (Point2D neighbor : points)
+        {
+            if (nearestNeighbor == null ||
+                    neighbor.distanceTo(p) < nearestNeighbor.distanceTo(p))
+            {
+                nearestNeighbor = neighbor;
+            }
+        }
+        
+        return nearestNeighbor;
     }
 
     // unit testing of the methods (optional) 
